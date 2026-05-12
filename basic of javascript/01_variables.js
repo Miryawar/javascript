c4c0b20144ca7745a814ccf838eb89207bb401e9
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-12 10:44:51 +0000

    name changed

---
+++ basic of javascript/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345;
+let accountName = "yawar";
+var accountBalance = 1000;
+console.log(accountId);
+console.table([accountId, accountName, accountBalance]);
+
+// accountId = 43521; not allowed
+accountName = "yawar mir";
+accountBamance = 2000;
+console.table({accountId, accountName, accountBalance});
+
---
+++ basic of javascript/03_datatypeConversion.js
@@ -0,0 +1,14 @@
+let score = "20aa"
+
+console.log(typeof score) //string
+console.log(typeof(score)) //string
+
+let value = Number(score) //conversion into a number
+console.log(typeof value)  //check whether it is convertesd into a num
+
+console.log(value)
+let number = 22
+console.log(typeof number)
+let conString = String(number)
+console.log(typeof conString)
+
