ea7dced1a93451808ff969f84e4e9156de9c3d53
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-12 10:40:58 +0000

    add files

---
+++ basic of javascript/02-datatypes.js
@@ -0,0 +1,11 @@
+let name ="yawar"//string
+
+let age =21 //number
+
+let isstudent = true //boolean
+
+console.log(name, age, isstudent)
+console.table({name, age, isstudent})
+//typeof operator
+console.log(typeof name)
+//typeof null is object in js.
