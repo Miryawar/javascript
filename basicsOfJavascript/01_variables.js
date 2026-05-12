54b57a0d3de76d586522b3453d4029969ea769e0
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-12 15:53:11 +0000

    about variables

--- basicsOfJavascript/01_variables.js
+++ basicsOfJavascript/01_variables.js
@@ -1,11 +1,18 @@
+//defining variables using var let and const
 const accountId = 12345;
 let accountName = "yawar";
 var accountBalance = 1000;
 console.log(accountId);
+console.log(accountName);
+console.log(accountBalance);
 console.table([accountId, accountName, accountBalance]);
+
+// accountId = 54321; not allowed   
 
 // accountId = 43521; not allowed
 accountName = "yawar mir";
-accountBamance = 2000;
+accountBalance = 2000;
+console.log(accountName);
+console.log(accountBalance);
 console.table({accountId, accountName, accountBalance});
 
