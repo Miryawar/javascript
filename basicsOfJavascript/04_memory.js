a48ad9982cd6b491bb55acaeeb568b8b5c388ab7
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-12 15:55:32 +0000

    stack,heap memory

--- basicsOfJavascript/04_memory.js
+++ basicsOfJavascript/04_memory.js
@@ -1,12 +1,12 @@
 //stack (primitives) and heap (objects/non-primitive)
-//passing by value(stack memory)
+//passing by value(stack memory no change to original value)
 let name = "yawar"
 let anothername = name
 anothername = "mir"
 
 console.log(name)
 console.log(anothername)
-//passing by reference(heap memory)
+//passing by reference(heap memory change reflects to original value)
 let user1 = {
     name : "ahmad",
     agr : 20
