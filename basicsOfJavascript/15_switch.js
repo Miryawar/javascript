321efb7c905d465de6f9cf8e637c83161e8ff804
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-13 10:53:48 +0000

    switch case

---
+++ basicsOfJavascript/15_switch.js
@@ -0,0 +1,27 @@
+
+const month = 3
+
+switch (month){
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("febuary");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    default:
+        console.log("enter valid number");
+        break;
+        
+}
