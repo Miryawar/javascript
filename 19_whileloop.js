b36ff8da8962da0147d670cb8d7b415ba66dbcc3
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-13 18:38:43 +0000

    about while loop

---
+++ 19_whileloop.js
@@ -0,0 +1,20 @@
+// while loop
+let index = 0
+while(index <= 10){
+    console.log(`value of index is ${index}`);
+    index++
+}
+
+let array = ["shop", "kiran", "hardware", "repairing"]
+ let arr = 0
+ while(arr < array.length){
+    console.log(`value of array si ${array[arr]}`);
+    arr++
+ }
+  //do while loop
+ let score = 1
+ do{
+    console.log(`score is ${score}`);
+    score++
+ }
+ while(score <= 10)
