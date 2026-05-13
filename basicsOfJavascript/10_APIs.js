1834fdfe5bab3c671ba39075d20c7bdbc79c073b
Yawar Fayaz Mir <miryawer990@gmail.com>
2026-05-13 06:23:35 +0000

    about API

---
+++ basicsOfJavascript/10_APIs.js
@@ -0,0 +1,11 @@
+//creating json object 
+{
+    "name" : "mirahmad",
+    "course" : "english",
+    "price" : "free"
+}
+
+//we also get api details in json format in the form of array
+[
+    {},{},{}
+]
